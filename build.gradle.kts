73aac2faded28c32881fe69e8f93400d2dd98dd7
AndreyBogdanov <andrey.bogdanov@cobrainer.com>
2024-10-09 13:20:58 +0200

    Use library published to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.ktor.client.logging)
             implementation(libs.logback)
-            implementation("com.bay:generic-ai:0.1.0")
+            implementation("io.github.bay73:generic-ai:0.2.1")
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
